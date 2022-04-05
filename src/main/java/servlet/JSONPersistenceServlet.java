6bcadeb04e8cf33b171927a9a271496a1ed08c38
Rijan Timsina <regiontimsina@gmail.com>
2022-04-05 11:10:40 -0400

    Update JSONPersistenceServlet.java

--- src/main/java/servlet/JSONPersistenceServlet.java
+++ src/main/java/servlet/JSONPersistenceServlet.java
@@ -1,4 +1,5 @@
 package servlet;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -32,13 +33,16 @@ requires Gson in your pom.xml
 */
 import com.google.gson.Gson;
 
-@WebServlet(name = "JSONPersistence", urlPatterns = {"/json"})
-public class JSONPersistenceServlet extends HttpServlet{
-  static enum Data {AGE, NAME};
+@WebServlet(name = "JSONPersistence", urlPatterns = { "/json" })
+public class JSONPersistenceServlet extends HttpServlet {
+  static enum Data {
+    AGE, NAME
+  };
+
   static String RESOURCE_FILE = "entries.json";
 
-  static String Domain  = "";
-  static String Path    = "/";
+  static String Domain = "";
+  static String Path = "/";
   static String Servlet = "json";
 
   // Button labels
@@ -49,70 +53,69 @@ public class JSONPersistenceServlet extends HttpServlet{
     Integer age;
   }
 
-  public class Entries{
+  public class Entries {
     List<Entry> entries;
   }
 
-  public class EntryManager{
+  public class EntryManager {
     private String filePath = null;
 
     public void setFilePath(String filePath) {
-        this.filePath = filePath;
+      this.filePath = filePath;
     }
-    public Entries save(String name, Integer age){
+
+    public Entries save(String name, Integer age) {
       Entries entries = getAll();
       Entry newEntry = new Entry();
       newEntry.name = name;
       newEntry.age = age;
       entries.entries.add(newEntry);
-      try{
+      try {
         FileWriter fileWriter = new FileWriter(filePath);
         new Gson().toJson(entries, fileWriter);
         fileWriter.flush();
         fileWriter.close();
-      }catch(IOException ioException){
+      } catch (IOException ioException) {
         return null;
       }
 
       return entries;
     }
 
-    private Entries getAll(){
-      Entries entries =  entries = new Entries();
+    private Entries getAll() {
+      Entries entries = entries = new Entries();
       entries.entries = new ArrayList();
 
-      try{
+      try {
         File file = new File(filePath);
-        if(!file.exists()){
+        if (!file.exists()) {
           return entries;
         }
 
-        BufferedReader bufferedReader =
-          new BufferedReader(new FileReader(file));
-        Entries readEntries =
-          new Gson().fromJson(bufferedReader, Entries.class);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        Entries readEntries = new Gson().fromJson(bufferedReader, Entries.class);
 
-        if(readEntries != null && readEntries.entries != null){
+        if (readEntries != null && readEntries.entries != null) {
           entries = readEntries;
         }
         bufferedReader.close();
 
-      }catch(IOException ioException){
+      } catch (IOException ioException) {
       }
 
       return entries;
     }
 
-    public String getAllAsHTMLTable(Entries entries){
+    public String getAllAsHTMLTable(Entries entries) {
       StringBuilder htmlOut = new StringBuilder("<table>");
       htmlOut.append("<tr><th>Name</th><th>Age</th></tr>");
-      if(entries == null
-          || entries.entries == null || entries.entries.size() == 0){
+      if (entries == null
+          || entries.entries == null || entries.entries.size() == 0) {
         htmlOut.append("<tr><td>No entries yet.</td></tr>");
-      }else{
-        for(Entry entry: entries.entries){
-           htmlOut.append(
-           "<tr><td>"+entry.name+"</td><td>"+entry.age+"</td></tr>");
+      } else {
+        for (Entry entry : entries.entries) {
+          htmlOut.append(
+              "<tr><td>" + entry.name + "</td><td>" + entry.age + "</td></tr>");
         }
       }
       htmlOut.append("</table>");
@@ -121,155 +124,156 @@ public class JSONPersistenceServlet extends HttpServlet{
 
   }
 
-  /** *****************************************************
-   *  Overrides HttpServlet's doPost().
-   *  Converts the values in the form, performs the operation
-   *  indicated by the submit button, and sends the results
-   *  back to the client.
-  ********************************************************* */
+  /**
+   * *****************************************************
+   * Overrides HttpServlet's doPost().
+   * Converts the values in the form, performs the operation
+   * indicated by the submit button, and sends the results
+   * back to the client.
+   */
   @Override
-  public void doPost (HttpServletRequest request, HttpServletResponse response)
-     throws ServletException, IOException
-  {
-     String name = request.getParameter(Data.NAME.name());
-     String rawAge = request.getParameter(Data.AGE.name());
-     Integer age = null;
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    String name = request.getParameter(Data.NAME.name());
+    String rawAge = request.getParameter(Data.AGE.name());
+    Integer age = null;
 
-     String error = "";
-     if(name == null){
-       error= "<li>Name is required</li>";
-       name = "";
-     }
+    String error = "";
+    if (name == null) {
+      error = "<li>Name is required</li>";
+      name = "";
+    }
 
-     if(rawAge == null){
-       error+= "<li>Age is required.<li>";
-       rawAge = "";
-     }else{
-          try{
-            age=new Integer(rawAge);
-            if(age<1){
-                error+= "<li>Age must be an integer greater than 0.</li>";
-                rawAge = "";
-            }else{
-              if(age>150){
-                  error+= "<li>Age must be an integer less than 150.</li>";
-                  rawAge = "";
-              }
-            }
-          }catch (Exception e) {
-            error+= "<li>Age must be an integer greater than 0.</li>";
+    if (rawAge == null) {
+      error += "<li>Age is required.<li>";
+      rawAge = "";
+    } else {
+      try {
+        age = new Integer(rawAge);
+        if (age < 1) {
+          error += "<li>Age must be an integer greater than 0.</li>";
+          rawAge = "";
+        } else {
+          if (age > 150) {
+            error += "<li>Age must be an integer less than 150.</li>";
             rawAge = "";
           }
-     }
+        }
+      } catch (Exception e) {
+        error += "<li>Age must be an integer greater than 0.</li>";
+        rawAge = "";
+      }
+    }
 
-     response.setContentType("text/html");
-     PrintWriter out = response.getWriter();
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
 
-     if (error.length() == 0){
-       EntryManager entryManager = new EntryManager();
-       entryManager.setFilePath(RESOURCE_FILE);
-       Entries newEntries=entryManager.save(name, age);
+    if (error.length() == 0) {
+      EntryManager entryManager = new EntryManager();
+      entryManager.setFilePath(RESOURCE_FILE);
+      Entries newEntries = entryManager.save(name, age);
 
-       printHead(out);
-       if(newEntries ==  null){
-         error+= "<li>Could not save entry.</li>";
-         printBody(out, name, rawAge, error);
-       }else{
-         printResponseBody(out, entryManager.getAllAsHTMLTable(newEntries));
-       }
-       printTail(out);
-     }else{
-       printHead(out);
-       printBody(out, name, rawAge, error);
-       printTail(out);
-     }
-
+      printHead(out);
+      if (newEntries == null) {
+        error += "<li>Could not save entry.</li>";
+        printBody(out, name, rawAge, error);
+      } else {
+        printResponseBody(out, entryManager.getAllAsHTMLTable(newEntries));
+      }
+      printTail(out);
+    } else {
+      printHead(out);
+      printBody(out, name, rawAge, error);
+      printTail(out);
+    }
 
   }
 
-  /** *****************************************************
-   *  Overrides HttpServlet's doGet().
-   *  Prints an HTML page with a blank form.
-  ********************************************************* */
+  /**
+   * *****************************************************
+   * Overrides HttpServlet's doGet().
+   * Prints an HTML page with a blank form.
+   */
   @Override
-  public void doGet (HttpServletRequest request, HttpServletResponse response)
-         throws ServletException, IOException{
-     response.setContentType("text/html");
-     PrintWriter out = response.getWriter();
-     printHead(out);
-     printBody(out, "", "", "");
-     printTail(out);
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html");
+    PrintWriter out = response.getWriter();
+    printHead(out);
+    printBody(out, "", "", "");
+    printTail(out);
   }
 
-  /** *****************************************************
-   *  Prints the <head> of the HTML page, no <body>.
-  ********************************************************* */
-  private void printHead (PrintWriter out){
-     out.println("<html>");
-     out.println("");
-     out.println("<head>");
-     out.println("<title>JSON File Persistence Example</title>");
-     // Put the focus in the name field
-     out.println ("<script>");
-     out.println ("  function setFocus(){");
-     out.println ("    document.persist2file.NAME.focus();");
-     out.println ("  }");
-     out.println ("</script>");
-     out.println("</head>");
-     out.println("");
+  /**
+   * *****************************************************
+   * Prints the <head> of the HTML page, no <body>.
+   */
+  private void printHead(PrintWriter out) {
+    out.println("<html>");
+    out.println("");
+    out.println("<head>");
+    out.println("<title>JSON File Persistence Example</title>");
+    // Put the focus in the name field
+    out.println("<script>");
+    out.println("  function setFocus(){");
+    out.println("    document.persist2file.NAME.focus();");
+    out.println("  }");
+    out.println("</script>");
+    out.println("</head>");
+    out.println("");
   }
 
-  /** *****************************************************
-   *  Prints the <BODY> of the HTML page
-  ********************************************************* */
-  private void printBody (
-    PrintWriter out, String name, String age, String error){
+  /**
+   * *****************************************************
+   * Prints the <BODY> of the HTML page
+   */
+  private void printBody(
+      PrintWriter out, String name, String age, String error) {
     out.println("<body onLoad=\"setFocus()\">");
     out.println("<p>");
     out.println(
-      "A simple example that demonstrates how to persist JSON data to a file"
-      );
+        "A simple example that demonstrates how to persist JSON data to a file");
     out.println("</p>");
 
-    if(error != null && error.length() > 0){
+    if (error != null && error.length() > 0) {
       out.println(
-      "<p style=\"color:red;\">Please correct the following and resubmit.</p>"
-      );
+          "<p style=\"color:red;\">Please correct the following and resubmit.</p>");
       out.println("<ol>");
       out.println(error);
       out.println("</ol>");
     }
 
-    out.print  ("<form name=\"persist2file\" method=\"post\"");
-    out.println(" action=\""+Domain+Path+Servlet+"\">");
+    out.print("<form name=\"persist2file\" method=\"post\"");
+    out.println(" action=\"" + Domain + Path + Servlet + "\">");
     out.println("");
     out.println(" <table>");
     out.println("  <tr>");
     out.println("   <td>Name:</td>");
-    out.println("   <td><input type=\"text\" name=\""+Data.NAME.name()
-      +"\" value=\""+name+"\" size=30 required></td>");
+    out.println("   <td><input type=\"text\" name=\"" + Data.NAME.name()
+        + "\" value=\"" + name + "\" size=30 required></td>");
     out.println("  </tr>");
     out.println("  <tr>");
     out.println("   <td>Age:</td>");
-    out.println("   <td><input type=\"text\"  name=\""+Data.AGE.name()
-      +"\" oninput=\"this.value=this.value.replace(/[^0-9]/g,'');\" value=\""
-      +age+"\" size=3 required></td>");
+    out.println("   <td><input type=\"text\"  name=\"" + Data.AGE.name()
+        + "\" oninput=\"this.value=this.value.replace(/[^0-9]/g,'');\" value=\""
+        + age + "\" size=3 required></td>");
     out.println("  </tr>");
     out.println(" </table>");
     out.println(" <br>");
     out.println(" <br>");
     out.println(" <input type=\"submit\" value=\"" + OperationAdd
-      + "\" name=\"Operation\">");
+        + "\" name=\"Operation\">");
     out.println(" <input type=\"reset\" value=\"Reset\" name=\"reset\">");
     out.println("</form>");
     out.println("");
     out.println("</body>");
   }
 
-  /** *****************************************************
-   *  Prints the <BODY> of the HTML page with persisted entries
-  ********************************************************* */
-  private void printResponseBody (PrintWriter out, String tableString){
+  /**
+   * *****************************************************
+   * Prints the <BODY> of the HTML page with persisted entries
+   */
+  private void printResponseBody(PrintWriter out, String tableString) {
     out.println("<body>");
     out.println("<p>");
     out.println("A simple example that shows entries from a JSON file");
@@ -280,11 +284,12 @@ public class JSONPersistenceServlet extends HttpServlet{
     out.println("</body>");
   }
 
-  /** *****************************************************
-   *  Prints the bottom of the HTML page.
-  ********************************************************* */
-  private void printTail (PrintWriter out){
-     out.println("");
-     out.println("</html>");
+  /**
+   * *****************************************************
+   * Prints the bottom of the HTML page.
+   */
+  private void printTail(PrintWriter out) {
+    out.println("");
+    out.println("</html>");
   }
 }
