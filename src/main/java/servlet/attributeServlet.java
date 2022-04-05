37d306df3e36c5b2decb89905fd35e4d5232eedf
Rijan Timsina <regiontimsina@gmail.com>
2022-04-05 11:05:03 -0400

    Update attributeServlet.java

--- src/main/java/servlet/attributeServlet.java
+++ src/main/java/servlet/attributeServlet.java
@@ -1,13 +1,17 @@
 // From "Professional Java Server Programming", Patzer et al.,
 package servlet;
-// Import Servlet Libraries
-import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.*;
-
 // Import Java Libraries
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Enumeration;
+
+// Import Servlet Libraries
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet(name = "attributeServlet", urlPatterns = {"/attribute"})
 public class attributeServlet extends HttpServlet
